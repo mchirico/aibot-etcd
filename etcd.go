0b8a52d926ef977da80cb4cf77093ce41b45c211
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-11-29 09:17:48 -0500

    update-automated-save

--- etcd.go
+++ etcd.go
@@ -13,12 +13,10 @@ func D(subkey string) {
 	e, cancel := etcdutils.NewETC("server")
 	defer cancel()
 
-
-
 	now := time.Now()
-	msg := fmt.Sprintf("{email: %q, timestamp: %q}",subkey,now)
-	key := fmt.Sprintf("aibot-gmail/%s",subkey)
-	key2 := fmt.Sprintf("aibot-gmail/last/%s",subkey)
+	msg := fmt.Sprintf("{email: %q, timestamp: %q}", subkey, now)
+	key := fmt.Sprintf("aibot-gmail/%s", subkey)
+	key2 := fmt.Sprintf("aibot-gmail/last/%s", subkey)
 
 	e.PutWithLease(key, msg, 300*600)
 	e.PutWithLease(key2, msg, 300*600)
@@ -30,15 +28,10 @@ func Query() {
 	e, cancel := etcdutils.NewETC("server")
 	defer cancel()
 
-
-
-
 	result, _ := e.GetWithPrefix("aibot-gmail")
-
 
 	for i, v := range result.Kvs {
 		log.Printf("result.Kvs[%d]: %s, ver: %d,  lease: %d\n", i, v.Value, v.Version, v.Lease)
 	}
-
 
 }
--- etcd_test.go
+++ etcd_test.go
@@ -1,7 +1,6 @@
 package etcd
 
 import (
-
 	"testing"
 )
 
